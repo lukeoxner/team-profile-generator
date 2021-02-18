24467c4b0f6c2a09a69ae24e8c21f0574e1a7d00
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-17 17:19:27 -0700

    working on generateProfile function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // set up our required Node modules
+const { profile } = require('console');
 const fs = require('fs');
 const inquirer = require('inquirer');
 // const { inherits } = require('util');
 
+// declare globals to store manager, engineers, interns
 let manager;
 const engineers = [];
 const interns = [];
@@ -180,7 +182,117 @@ function createIntern() {
 
 // declare function used for generating the team profile
 function generateProfile() {
-	console.log('time to generate a profile!');
+	fs.writeFile(
+		'team-profile.html',
+		`<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+		<title>The Squaaad</title>
+	
+		<style>
+	
+		header {
+			background-color: #2a9d8f;
+			font-family: 'Pacifico', cursive;
+			color: #f1faee;
+			text-shadow: 2px 2px #000000;
+		}
+	
+		li {
+			font-size: 14px;
+		}
+	
+		a {
+			color: #2a9d8f;
+			text-decoration: none;
+		}
+	
+		.key {
+			font-weight: 700;
+		}
+	
+		.card {
+			box-shadow: 3px 3px 5px #000;
+			background-color: #f7f7f7;
+		}
+	
+		.card-header {
+			background-color: #264653;
+			color: #f1faee;
+		}
+	
+		</style>
+	
+	</head>
+	
+	<body>
+		<header>
+			<div class="container">
+				<div class="row py-5 mb-5">
+					<div class="col-12 text-center">
+						<h1>The Squad</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+	
+		<main>
+			<div class="container">
+				<div class="row d-flex justify-content-center card-element">
+	
+					<div class="col-12 col-md-6 col-lg-4 card m-3 p-0" style="width: 18rem;">
+						<div class="card-header">
+							<h5 class="card-title">Elon</h5>
+							<h6 class="card-text">Manager</h6>
+						</div>
+						<ul class="list-group list-group m-4">
+							<li class="list-group-item"><span class="key">ID: </span>1</li>
+							<li class="list-group-item"><span class="key">Email: </span><a href="mailto:elon@spacex.com">elon@spacex.com</a></li>
+							<li class="list-group-item"><span class="key">Office: </span>1</li>
+						</ul>
+					</div>
+	
+					<div class="col-12 col-md-6 col-lg-4 card m-3 p-0" style="width: 18rem;">
+						<div class="card-header">
+							<h5 class="card-title">Franklin</h5>
+							<h6 class="card-text">Engineer</h6>
+						</div>
+						<ul class="list-group list-group m-4">
+							<li class="list-group-item"><span class="key">ID: </span>2</li>
+							<li class="list-group-item"><span class="key">Email: </span><a href="mailto:franklin@spacex.com">franklin@spacex.com</a></li>
+							<li class="list-group-item"><span class="key">GitHub: </span><a href="https://github.com/franklinthedev">FranklinTheDev</a></li>
+						</ul>
+					</div>
+					
+					<div class="col-12 col-md-6 col-lg-4 card m-3 p-0" style="width: 18rem;">
+						<div class="card-header">
+							<h5 class="card-title">Ryan</h5>
+							<h6 class="card-text">Intern</h6>
+						</div>
+						<ul class="list-group list-group m-4">
+							<li class="list-group-item"><span class="key">ID: </span>3</li>
+							<li class="list-group-item"><span class="key">Email: </span><a href="mailto:ryan@spacex.com">ryan@spacex.com</a></li>
+							<li class="list-group-item"><span class="key">School: </span>University of Denver</li>
+						</ul>
+					</div>
+	
+				</div>
+			</div>
+		</main>
+	
+	
+	</body>
+	</html>`,
+		(err) =>
+			// If there is an error, console log it. Otherwise, console log a success message
+			err ? console.error(err) : console.log('Team Profile generated!')
+	);
 }
 
 // Kick everything off by calling the createManager function

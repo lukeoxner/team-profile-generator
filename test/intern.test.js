1f6df79a0c0a455a4cd1710f3693cb25856f5f39
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-18 20:16:07 -0700

    finish intern test functionality

--- test/intern.test.js
+++ test/intern.test.js
@@ -10,4 +10,23 @@ describe('Intern class', () => {
 			expect('name' in intern).toEqual(true);
 		});
 	});
+
+	// second test
+	describe('getSchool method', () => {
+		it('Should return intern school', () => {
+			const intern = new Intern();
+			const school = intern.school;
+
+			expect(intern.getSchool()).toEqual(school);
+		});
+	});
+
+	// third test
+	describe('getRole method', () => {
+		it('Should return intern role', () => {
+			const intern = new Intern();
+
+			expect(intern.getRole()).toEqual('Intern');
+		});
+	});
 });

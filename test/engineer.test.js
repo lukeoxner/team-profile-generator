855b891cf404f92a7d72247c83be1fe034f47979
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-18 20:07:42 -0700

    work on engineer test function

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -3,11 +3,22 @@ const Engineer = require('../Engineer');
 const Manager = require('../Engineer');
 
 describe('Engineer class', () => {
+	// first test
 	describe('Initialization', () => {
 		it('Should create new engineer', () => {
 			const engineer = new Engineer();
 
 			expect('name' in engineer).toEqual(true);
+		});
+	});
+
+	// second test
+	describe('getGithub method', () => {
+		it('Should return engineer GitHub', () => {
+			const engineer = new Engineer();
+			const github = engineer.github;
+
+			expect(engineer.getGithub()).toEqual(github);
 		});
 	});
 });

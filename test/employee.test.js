6df29388dbc2a4b0831c0cb663b679d094fd9165
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-02-18 19:51:07 -0700

    write employee unit tests

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,12 +1,49 @@
 // requiring in our index.js file
-const Employee = require('../app');
+const Employee = require('../Employee');
 
 describe('Employee class', () => {
+	// first test
 	describe('Initialization', () => {
 		it('Should create new employee', () => {
 			const employee = new Employee();
 
 			expect('name' in employee).toEqual(true);
+		});
+	});
+
+	// second test
+	describe('getName method', () => {
+		it('Should return employee name', () => {
+			const employee = new Employee();
+			const name = employee.name;
+
+			expect(employee.getName()).toEqual(name);
+		});
+	});
+	// third test
+	describe('getId method', () => {
+		it('Should return employee ID', () => {
+			const employee = new Employee();
+			const id = employee.id;
+
+			expect(employee.getId()).toEqual(id);
+		});
+	});
+	// fourth test
+	describe('getEmail method', () => {
+		it('Should return employee email', () => {
+			const employee = new Employee();
+			const email = employee.email;
+
+			expect(employee.getEmail()).toEqual(email);
+		});
+	});
+	// fifth test
+	describe('getRole method', () => {
+		it('Should return employee role', () => {
+			const employee = new Employee();
+
+			expect(employee.getRole()).toEqual('Employee');
 		});
 	});
 });
